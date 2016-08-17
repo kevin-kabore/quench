513bc8ccbc817f4256377e7b9887681ee3e606aa
Adrianna Marini <adrimarini>
2016-08-17 09:52:32 -0700

    attempts js drink dropdown but no work

---
+++ public/javascripts/app.js
@@ -0,0 +1,16 @@
+// console.log("im running in app.js")
+//  var $form;
+//  var $drinkType;
+//
+//
+// $(document).ready(function() {
+//   $form = $('#newDrinkForm');
+//   $drinkType = $('#drinkTypeMenu');
+// })
+//
+// $form.on('submit', function(evt) {
+//   evt.preventDefault()
+//   var newDrink = {
+//     drinkType: $drinkType.val()
+//   }
+// })
