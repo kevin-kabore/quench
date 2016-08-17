f71de599e0259ef35c3def0dfb645a897676d180
Christian Perez <chris.dillon92@gmail.com>
2016-08-17 10:15:37 -0700

    adds reviews to drink

--- models/Drink.js
+++ models/Drink.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-console.log("Requires model");
 
-var reviewSchema = mongoose.Schema({
+var reviewSchema = new Schema({
   rating: {type: Number, required: true, min: 0, max: 5 },
   bitter: {type: Number, required: true, min: 0, max: 5},
   sweet: {type: Number, required: true, min: 0, max: 5},
@@ -12,19 +11,15 @@ var reviewSchema = mongoose.Schema({
   thoughts: {type: String, required: false},
   user: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   venue: [{type: mongoose.Schema.Types.ObjectId, ref: 'Venue'}]
-  //  stories : [{ type: Schema.Types.ObjectId, ref: 'Story' }]
 });
 
-var drinkSchema = mongoose.Schema({
+var drinkSchema = new Schema({
   drinkType: {type: String, enum:['Wine', 'Beer', 'Cocktails'], required: true},
   alcohol: {type: Boolean, required: true, default: false},
   drinkName: {type: String, required: true},
   venue: [{type: mongoose.Schema.Types.ObjectId, ref: 'Venue'}],
   reviews: [reviewSchema]
-  // stories : [{ type: Schema.Types.ObjectId, ref: 'Story' }]
 });
-
-var Review = mongoose.model('Review', reviewSchema);
 
 var Drink = mongoose.model('Drink', drinkSchema);
 module.exports = Drink;
